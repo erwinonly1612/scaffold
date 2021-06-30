38b013deaa6daf389ed47e25b0bacbc4f7b051de
Erwin <erwinhuang16@gmail.com>
2021-06-30 15:42:06 +0000

    Adding Azure Version

--- hello.py
+++ hello.py
@@ -4,4 +4,5 @@ def add(x, y):
 #var=
 result = add(1, 2)
 
-print(f"this is the sum: 1, 2, {result}")
+print(f"this is the sum: 1, 2, {result}") #not compatible with python version below 3.6
+print("this is the sum: 1, 2, {result}") #compatible with python version below 3.6
